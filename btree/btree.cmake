074d60021ff436b2b6bee619cc9b413c0dad834a
KolKir <kirill@c-vision.com.ua>
2014-02-08 14:18:30 +0200

    Update structure

---
+++ btree/btree.cmake
@@ -0,0 +1,11 @@
+set(BTREE_LIB_SOURCES
+    ${PROJECT_SOURCE_DIR}/btree/exceptions.h
+    ${PROJECT_SOURCE_DIR}/btree/btree.h
+    ${PROJECT_SOURCE_DIR}/btree/btreenode.h
+    ${PROJECT_SOURCE_DIR}/btree/defpack.h
+    ${PROJECT_SOURCE_DIR}/btree/keypack.h
+    ${PROJECT_SOURCE_DIR}/btree/filelocation.h
+    ${PROJECT_SOURCE_DIR}/btree/podfilerecord.h
+    ${PROJECT_SOURCE_DIR}/btree/recordfile.h
+    ${PROJECT_SOURCE_DIR}/btree/stream.h)  
+    
